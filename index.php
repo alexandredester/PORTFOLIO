c51b98683153b5de30cb8f733df271a2f159bf76
Alexandre <alexandre.dester01@gmail.com>
2019-02-13 23:55:05 +0100

    Portofolio

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Alexandre Dester</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link rel="shortcut icon" type="image/png" href="image/favicon.ico"/>
+</head>
+<body>
+
+                            <!--Menu-->
+  <header id="header">
+    <div class="container">
+
+    <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">Home</a></li>
+          <li><a href="#Mon-Pacours">Mon Pacours</a></li>
+            <li><a href="#SKILLS">SKILLS</a></li>
+          <li><a href="#portfolio">Mes Réalisations</a></li>
+          <li><a href="#Contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+                         <!--Présentation-->
+  <section id="hero">
+    <div class="hero-container">
+      <h1>BIENVENUE SUR MON PORTFOLIO</h1>
+      <h2>Alexandre Dester Développeur - Web</h2>
+      <a href="#Mon-Pacours" class="btn-get-started">Go</a>
+    </div>
+  </section>
+
+  <main id="main">
+
+                        <!--Parcours-->
+    <section id="Mon-Pacours">
+      <div class="container">
+        <div class="row about-container">
+
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <h2 class="présentation">QUI SUIS-JE ?</h2>
+            <p class="parcours">Je m’appelle Alexandre DESTER j'ai 30 ans et je suis passionné par l'informatique
+            et les <strong>nouvelles technologies</strong>, je me suis naturellement dirigé vers cette voie, en suivant  une formation de 3 mois de
+            "développeur / intégrateur d’application web "  à la  <a title="3W Academy" href="https://3wa.fr/"><strong class="skillsembodized">3W ACADEMY</strong></a>,
+            formation très riche qui a confirmé mes aptitudes et considérablement accru mes compétences en <strong>développement web</strong> (HTML, CSS, JS, PHP & SQL).
+            J'ai parfaitement conscience qu'il s'agit d'un secteur en constante évolution et que cela implique d'assimiler sans cesse de <strong> nouvelles techniques</strong>.
+            Cela tombe bien, j'aime ça ! Je continue le développement de mes compétences grâce au site <a title="openclassrooms" href="https://openclassrooms.com/"><strong class="skil">Openclassrooms</strong></a>
+
+
+
+         <div class="col-lg-3 col-md-6 background order-lg-2 order-1 wow fadeInRight"></div>
+              <img class="bouille" src="image/alexandre_dester.jpg" alt="alexandre dester" /></a>
+        </div>
+
+      </div>
+    </section>
+
+                          <!--langage-->
+    <section id="SKILLS">
+      <div class="container wow fadeIn">
+        <div class="section-header">
+          <h3 class="section-title">SKILLS</h3>
+          <p class="section-description">Mes Compétences Back-End et Front-End</p>
+        </div>
+        <div class="row counters">
+
+          <div class="html">
+               <img src="image/Langage/html.png" alt="html-img"title="HTML5"></a>
+          </div>
+          <div class="css">
+              <img src="image/Langage/css.png" alt="css-img"title="CSS3"></a>
+          </div>
+          <div class="js">
+              <img src="image/Langage/js.png" alt="js-img"title="JavaScript"></a>
+          </div>
+          <div class="php">
+              <img src="image/Langage/php.png" alt="php-img"title="PHP"></a>
+          </div>
+          <div class="sql">
+              <img src="image/Langage/sql.png" alt="sql-img"title="SQL"></a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+   <section id="call-to-action">
+      <div class="container wow fadeIn">
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-left">
+          </div>
+                      <!--CV-->
+
+         <div class="col-lg-3 cta-btn-container text-center">
+            <div class="cv">
+            <a href= "image/CV DESTER Alexandre..pdf" target= "_blank" class= "btn btn-secondary btn-parcours btnaligne" title= "CV de Alexandre Dester">CONSULTER MON CV</a>
+            <a href= "image/CV DESTER Alexandre..pdf" download= "CV DESTER Alexandre" class= "btn btn-secondary btn-download-cv btnaligne" title= "CV de Alexandre Dester">TELECHARGER MON CV</a>
+          </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+                      <!--Portfolio-->
+    <section id="portfolio">
+      <div class="container wow fadeInUp">
+        <div class="section-header">
+          <h3 class="section-title">Portfolio</h3>
+          <p class="section-description">Mes projets réalisés au cours de ma formation ou chez moi !</p>
+        </div>
+        <div class="row">
+
+          <div class="col-lg-12">
+            <ul id="portfolio-flters">
+              <li data-filter=".filter-Intégration, .filter-DEVELOPPEMENT" class="filter-TOUT">TOUT</li>
+              <li data-filter=".filter-Intégration">Intégration</li>
+              <li data-filter=".filter-DEVELOPPEMENT">DEVELOPPEMENT</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row" id="portfolio-wrapper">
+          <div class="col-lg-3 col-md-6 portfolio-item filter-Intégration">
+            <a href="">
+              <img src="image/gallery/img1.png" alt="">
+              <div class="details">
+                <h4>CUP OF TEA</h4>
+                <span> RESPONSIVE-RESET.CSS-FLEXBOX</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-Intégration">
+            <a href="">
+                <img src="image/gallery/img2.png" alt="">
+              <div class="details">
+                <h4>RED WOLF</h4>
+                <span>JQUERY-VIDEO-FLEXSLIDER</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-Intégration">
+            <a href="">
+                <img src="image/gallery/img3.png" alt="">
+              <div class="details">
+                <h4>PORTFOLIO JOHN DOE</h4>
+                <span>JQUERY-SVG/CANVAS-ANIMATIONS CSS</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-Intégration">
+            <a href="">
+              <img src="image/gallery/img5.png" alt="">
+              <div class="details">
+                <h4>YOGA ILA</h4>
+                <span>RESET/NORMALIZE-FLOAT</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-DEVELOPPEMENT">
+            <a href="">
+              <img src="image/gallery/img6.png" alt="">
+              <div class="details">
+                <h4>RESTO 3WA</h4>
+                <span>MVC-POO-MICRO-USER & ADMIN</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-DEVELOPPEMENT">
+            <a href="">
+                <img src="image/gallery/img7.png" alt="">
+              <div class="details">
+                <h4>MAGICAL SLATE</h4>
+                <span>JAVASCRIPT-EVENTLISTENER-POO</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-DEVELOPPEMENT">
+            <a href="">
+                <img src="image/gallery/img8.png" alt="">
+              <div class="details">
+                <h4>AGENCE IMMOBILIÈRE</h4>
+                <span>BACKGROUND VIDEO-FLEXBOX-</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-DEVELOPPEMENT">
+            <a href="">
+                <img src="image/gallery/img9.png" alt="">
+              <div class="details">
+                <h4>BICLOO</h4>
+                <span>API REST-AJAX-JQUERY</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 portfolio-item filter-DEVELOPPEMENT">
+            <a href="">
+                <img src="image/gallery/img10.png" alt="">
+              <div class="details">
+                <h4>APPLI FLICKR</h4>
+                <span>API-REST FLICKR-SILEX/TWIG-MVC/POO</span>
+              </div>
+            </a>
+          </div>
+        </div>
+    </section>
+
+
+                          <!--Contact-->
+        <section id="Contact">
+        <div class="container wow fadeInUp">
+        <div class="section-header">
+        <h3 class="section-title">Contact</h3>
+        <h4 class="job">Je suis disponible de suite, je suis à la recherche d'un CDD/CDI ou d'un contrat de professionnalisation dans la France entière.<h4>
+
+
+          </div>
+              </div>
+
+        <div class="container wow fadeInUp mt-5">
+          <div class="d-flex justify-content-center">
+            <a id="social" target="_blank" href="mailto:alexandre.dester01@gmail.com"><i class="fas fa-at"aria-hidden="true"></i></a>
+            <a id="social" target="_blank" href="tel:+33651698987"><i class="fas fa-phone-square"aria-hidden="true"></i></a>
+            <a id="social" target="_blank" href="https://www.facebook.com/alexou.dester"><i class="fab fa-facebook"aria-hidden="true"></i></a>
+            <a id="social" target="_blank" href="https://twitter.com/AlexandreDester"><i class="fab fa-twitter-square"aria-hidden="true"></i></a>
+            <a id="social" target="_blank" href="https://www.instagram.com/alexpley"><i class="fab fa-instagram"aria-hidden="true"></i></a>
+            <a id="social" target="_blank" href="https://www.linkedin.com/in/alexandre-dester-459a1975"><i class="fab fa-linkedin"aria-hidden="true"></i></a>
+            <a id="social" target="_blank" href="https://github.com/alexandredester"><i class="fab fa-github-square"aria-hidden="true"></i></a>
+        </div>
+            </div>
+              </section>
+
+
+                                <!-- Cookie -->
+            <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+                <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+              </div>
+                                <!-- copyright -->
+              <div class="footer copyright">     Copyright &copy; <?php echo date('Y'); ?> <a href="mention-légal.php">Alexandre Dester</a></div>
+
+                              <!-- GoogleAnalytics -->
+                    <script>
+                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                    ga('create', 'UA-91057246-1', 'auto');
+                    ga('send', 'pageview');
+                    </script>
+                                    <!-- Cookie -->
+                    <script>
+                    	window.addEventListener("load", function(){
+                    	window.cookieconsent.initialise({	"palette": {"popup": {"background": "#252e39"},	"button": {	"background": "#14a7d0"	}},
+                    	"showLink": false,	"content": {"message": "En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de [Cookies ou autres traceurs] pour vous proposer\n [Par exemple, des publicités ciblées adaptés à vos centres d’intérêts] et [Par exemple, réaliser des statistiques de visites].",
+                    	"dismiss": "ok "}})});
+                    </script>
+                        <!--Back-TO-TOP-->
+          <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+                      <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="contactform/contactform.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
